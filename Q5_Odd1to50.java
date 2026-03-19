b00837d418bfa840e41dfd9948e519d2eecf4602
pratikshivale <pratikshivale07@gmail.com>
2026-03-19 14:11:46 +0530

    Q5 Odd 1 to 50 and Q6 Print even or odd number code

---
+++ Q5_Odd1to50.java
@@ -0,0 +1,9 @@
+public class Q5_Odd1to50 {
+    public static void main(String[] args){
+        for(int i=1; i<=50; i++){
+            if(i%2 == 1){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ Q6_PrintEvenOrOdd.java
@@ -0,0 +1,13 @@
+public class Q6_PrintEvenOrOdd {
+    public static void main(String[] args){
+        for(int i=1;i<=50;i++){
+            if(i%2==0){
+                System.out.println(i + " Even");
+            }
+            else {
+                System.out.println(i + " Odd");
+
+            }
+        }
+    }
+}
