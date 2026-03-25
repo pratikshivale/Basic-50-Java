c70f214ec298e88d63f57e63aa71306f0ed6b72e
pratikshivale <pratikshivale07@gmail.com>
2026-03-25 16:26:58 +0530

    Q13 Inverted pyramid pattern print

---
+++ Q13_InvertedPyramid.java
@@ -0,0 +1,12 @@
+public class Q13_InvertedPyramid {
+    public static void main(String[]args){
+        for(int i=1; i<=5; i++){
+            for(int j = 1; j <= i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 2*(5-i)+1; j++){
+                System.out.print("*");
+            }System.out.println();;
+        }
+    }
+}
