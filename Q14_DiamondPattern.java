af21833c7d37f2b94b2346ae1f3601bbb21cdf08
pratikshivale <pratikshivale07@gmail.com>
2026-03-25 17:45:03 +0530

    Q14 Diamond pattern print

---
+++ Q14_DiamondPattern.java
@@ -0,0 +1,20 @@
+public class Q14_DiamondPattern {
+    public static void main(String[]args){
+        for(int i =1; i<=5; i++){
+            for(int j=1; j<=5-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print("*");
+            }System.out.println();
+
+        }
+        for(int i=4; i >=1;  i-- ){
+            for(int j=1; j<=5-i; j++){
+            System.out.print(" ");}
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+}
