ca7d341878435faea0ab925fcc92406eaa583c4c
pratikshivale <pratikshivale07@gmail.com>
2026-03-27 23:38:06 +0530

    Q16 Hollow right triangle pattern print

---
+++ Q16_HollowTriangle.java
@@ -0,0 +1,14 @@
+public class Q16_HollowTriangle {
+    public static void main(String[] args) {
+        for(int a = 1; a <= 5; a++){
+            for(int b = 1; b <= a; b++){
+                if(a==1||a==5||b==1||b==a){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+}
