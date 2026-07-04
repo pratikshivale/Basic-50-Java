bcc5964f9af6d9d46bb0c7804657bdcb74f7a399
pratikshivale <pratikshivale07@gmail.com>
2026-07-05 02:38:54 +0530

    These program for clear basic of variables

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+
+public static void main(String[] args) {
+
+    String name= "Pratik";
+    int age = 6;
+    double pi =  3.14159;
+    char gender = 'M';
+    boolean isAdmin = true;
+
+    System.out.println("My name is " + name + "and i am " + age + "years old " );
+    if(isAdmin){
+        System.out.println("I am an admin");
+    }
+    else{System.out.println("I am not an admin");}
+}
+}
